90ab887567913c7a47216df27ce3bc5802fc6bec
Elwyn <elwyn_de_neve@hotmail.com>
2023-04-25 09:49:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# demo-js-les-6
---
+++ src/app.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+// Verwijzing maken naar HTML elementen
+const inputField = document.getElementById( 'input-field' );
+const submitForm = document.getElementById( 'submit-form' );
+const recipeList = document.getElementById( 'recipe-list' );
+
+// Variabele voor input value
+// let inputValue = '';
+
+// Input event listener
+// inputField.addEventListener('input', (event)=>{
+//     inputValue = event.target.value;
+//     console.log(inputValue);
+// })
+
+// Edamam API variabelen
+const APP_ID = 'ee3c69b6';
+const APP_KEY = '2853c1f81651d74fa47d264e4c1da07e';
+const URI = `https://api.edamam.com/api/recipes/v2`;
+
+// API URL
+async function getRecipes() {
+    try {
+        const response = await axios.get(`${ URI }?type=public&q=${ inputField.value }&app_id=${ APP_ID }&app_key=${ APP_KEY }` );
+        const recipes = response.data.hits;
+        console.log( recipes );
+        recipes.map((item)=>{
+            const { label, url } = item.recipe;
+            recipeList.innerHTML += `<li><a class="link" href="${ url }" >${ label }</a></li>`
+        })
+    } catch ( e ) {
+        console.error( e )
+    }
+}
+
+// Button event listener
+submitForm.addEventListener( 'submit', function ( event ) {
+    event.preventDefault();
+    recipeList.innerHTML = null;
+    void getRecipes(inputField.value)
+    inputField.value = '';
+} );
